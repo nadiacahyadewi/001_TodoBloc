c7efbc7905ef788f7a512a9de8e72f76925228e5
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 09:14:58 +0700

    kode dikelas

---
+++ android/app/src/main/kotlin/nadiacahya/id/todo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package nadiacahya.id.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/bloc/todo_bloc.dart
@@ -0,0 +1,48 @@
+import 'package:bloc/bloc.dart';
+//import 'package:meta/meta.dart';
+import 'package:todo_app/model/todo.dart';
+
+part 'todo_event.dart';
+part 'todo_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
+    on<TodoEventAdd>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+        updatedTodos.add(
+          Todo(title: event.title, isCompleted: false, date: event.date),
+        );
+        emit(
+          TodoLoaded(
+            todos: updatedTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      }
+    });
+
+    on<TodoEventComplete>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+
+        if (event.index >= 0 && event.index < updatedTodos.length) {
+          updatedTodos[event.index] = Todo(
+            title: updatedTodos[event.index].title,
+            //isCompleted: !updatedTodos[event.index].isCompleted,
+            isCompleted: updatedTodos[event.index].isCompleted == true,
+            date: updatedTodos[event.index].date,
+          );
+          emit(
+            TodoLoaded(
+              todos: updatedTodos,
+              selectedDate: currentState.selectedDate,
+            ),
+          );
+        }
+      }
+    });
+  }
+}
