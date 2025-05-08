43752c2dd0ea9ee516a7a66d1e5779ea900ce5f0
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 09:47:21 +0700

    Menambahkan variabel final untuk GlobalKey dan TextEditingController

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -5,6 +5,8 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold();
   }
 }
