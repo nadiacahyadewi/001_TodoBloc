d23decda1d6c18cc0be35c02ce3fd875ccac93a2
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 17:54:27 +0700

    menghapus import 'package:todo_app/model/todo.dart';

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/bloc/todo_bloc.dart';
-import 'package:todo_app/model/todo.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
