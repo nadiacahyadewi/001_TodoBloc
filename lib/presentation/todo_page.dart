fbcacb3108363ec9f3a0b7a8778442227c668a72
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 09:59:52 +0700

    Membuat UI awal halaman Todo List dengan BlocBuilder untuk tanggal

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -7,6 +9,38 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('Selected Date'),
+                      BlocBuilder<TodoBloc, TodoState>(
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No date selected');
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
