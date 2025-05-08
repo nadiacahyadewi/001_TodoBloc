253cc9a1ccd3d9e0c2d5d4d0f90f71c4928bd6f1
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 10:17:36 +0700

    Menambahkan validasi input todo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -69,6 +69,12 @@ class TodoPage extends StatelessWidget {
                           labelText: 'Todo',
                           border: OutlineInputBorder(),
                         ),
+                        validator: (value){
+                          if  (value == null || value.isEmpty){
+                            return 'Please enter a todo';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ],
