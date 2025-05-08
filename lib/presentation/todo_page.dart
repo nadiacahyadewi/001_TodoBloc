90c94a94ea8d649b149f91d64e2c38a34cd90bd0
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-08 14:52:10 +0700

    menambah container untuk todo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -112,6 +112,14 @@ class TodoPage extends StatelessWidget {
                         itemCount: state.todos.length,
                         itemBuilder: (context, index) {
                           final todo = state.todos[index];
+                          return Container(
+                            margin: EdgeInsets.only(bottom: 8.0),
+                            padding: EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                              color: Colors.blue[50],
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          );
                         },
                       );
                     }
